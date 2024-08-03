7a13eedd0df6d448814eb1552b87111d84103ba1
piriyaluxan <sutheskuamrpiriyaluxan025@gmail.com>
2024-08-03 11:47:57 +0530

    #1-frontend initiated

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import Login from './pages/login'
+import Home from './pages/home'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path ='/login' element={<Login/>}></Route>
+            <Route path ='/home' element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
